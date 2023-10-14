6bdf5dae73046d836da8dd34f0dbcfb70439e1c1
Mariana Alves <102690791+mzayles@users.noreply.github.com>
2023-10-14 16:44:20 -0300

    Ex. 02

--- exercicios-fixacao.sql
+++ exercicios-fixacao.sql
@@ -1,7 +1,7 @@
 -- Ex. 01
 -- A)
 CREATE TABLE nomes (
-    nome VARCHAR(100)
+nome VARCHAR(100)
 );
 
 INSERT INTO nomes (nome) VALUES
@@ -19,8 +19,31 @@ SELECT LENGTH(nome) AS tamanho FROM nomes;
 -- D)
 SELECT CONCAT('Sr. ', nome) AS nome
 FROM nomes
-WHERE nome LIKE '%O';
-
+WHERE nome LIKE '%O'
+UNION
 SELECT CONCAT('Sra. ', nome) AS nome
 FROM nomes
 WHERE nome LIKE '%A';
+
+-- Ex. 02
+-- A)
+
+DROP TABLE produtos;
+CREATE TABLE produtos (
+produto VARCHAR(100),
+    preco DECIMAL(10, 2),
+    quantidade INT
+);
+
+INSERT INTO produtos VALUES
+('Base', '259.999', '100'),
+('Batom', '54.999', '80'),
+('Gloss', '69.999', '200'),
+('Blush', '99.999', '50');
+
+-- B)
+SELECT ROUND(preco, 2) AS preço_arredondado
+FROM produtos;
+
+-- C)
+SELECT ROUND(AVG(preco), 2) AS media_produto FROM produtos;
