7a9a8ec7a2da7f4efcd44041c601bfb7f09c3ffc
Mariana Alves <102690791+mzayles@users.noreply.github.com>
2023-10-24 22:56:42 -0300

    Ex. 02

--- trigger.sql
+++ trigger.sql
@@ -11,3 +11,12 @@ INSERT INTO Clientes (nome)
 VALUES('Gertrudes');
 
 SELECT * FROM Auditoria;
+
+-- Ex. 02
+CREATE TRIGGER exclusao_cliente BEFORE DELETE ON Clientes
+FOR EACH ROW
+    INSERT INTO Auditoria(mensagem)
+    VALUES('ATENÇÂO! Tentativa de exclusão de cliente.');
+
+DELETE FROM Clientes WHERE nome = 'Astrogilda';
+SELECT * FROM Auditoria;
